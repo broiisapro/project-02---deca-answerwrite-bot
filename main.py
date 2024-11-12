608ae9e85e79643cec9856bfc7403d3e207644c3
broiisapro <moksh.siruvani@gmail.com>
2024-11-12 09:56:18 -0500

    finished the code for this deca answerwrite bot

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ pyautogui.alert("ready to start? go to the deca answerwrite page and begin the e
 
 for x in range(100):
     location = pyautogui.locateCenterOnScreen('image.png', confidence=0.9)
+    pyautogui.click(location)
+
+    try:
+        click = pyautogui.locateCenterOnScreen('b.png', confidence=0.6)
+        pyautogui.click(click)
+    except:
+        try:
+            click = pyautogui.locateCenterOnScreen('c.png', confidence=0.6)
+            pyautogui.click(click)
+        except:
+            try:
+                click = pyautogui.locateCenterOnScreen('d.png', confidence=0.6)
+                pyautogui.click(click)
+            except:
+                continue
+    time.sleep(0.1)
+    next = pyautogui.locateCenterOnScreen('next.png', confidence=0.9)
+    pyautogui.click(next)
